5ca7583cda0cb7839e64eead204a54d360949c3d
pegazus-alpha <pourdebutantp@gmail.com>
2025-12-07 23:01:36 +0100

    correctif

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,4 +1,5 @@
 import type { WASocket } from '@whiskeysockets/baileys';
+import { jidNormalizedUser } from '@whiskeysockets/baileys';
 
 export const now = () => Date.now();
 
@@ -8,15 +9,16 @@ export const short = (s: string | undefined, l = 100) => {
 };
 
 export const jidToNumber = (jid: string) => {
-  return jid.split('@')[0] as string;
+  return jidNormalizedUser(jid).split('@')[0];
 };
 
 export const getUsername = async (sock: WASocket, jid: string): Promise<string> => {
     try {
-        const user = await sock.onWhatsApp(jid);
+        const normalizedJid = jidNormalizedUser(jid);
+        const user = await sock.onWhatsApp(normalizedJid);
         if (user && user.length > 0 && user[0] && user[0].exists) {
-            const fetchedUser = await sock.fetchContactInfo(jid);
-            return fetchedUser?.name || fetchedUser?.notify || jid.split('@')[0];
+            const fetchedUser = await sock.getContact(normalizedJid);
+            return fetchedUser?.name || fetchedUser?.notify || normalizedJid.split('@')[0];
         }
     } catch (e) {
         console.error("Erreur lors de la récupération du nom d'utilisateur:", e);
